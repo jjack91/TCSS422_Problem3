1f2ce4dcdca4cbf7fad08bbfb6cc027058389436
jjack91 <jjack91@uw.edu>
2017-10-19 10:19:20 -0700

    MLFQ partial implementation
    -Partially implemented the MLFQ into os_main.c
    -Altered the scheduler, isr, and dispatcher to accept a priority queue datatype
    -Partial implementation of marking process for termination, new process queue, and zombie queue

--- os_main.c
+++ os_main.c
@@ -2,12 +2,14 @@
 
 #include "pcb.h"
 #include "fifoqueue.h"
+#include "priority_queue.h"
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 int sys_stack = 0;
 int output_tick = 0;
+//int g_pid = 0;
 
 enum interrupt_type {TIMER, IO, EXCEPTION}; // IO and Exception states won't be used (yet),
 											// but includded for completeness
@@ -22,7 +24,9 @@ int main(void) {
 	
 	// List for processes
 	printf("Begin program\n");
-	FIFO_q_p jobs = fifo_q_new();
+	FIFO_q_p new_jobs = fifo_q_new();
+	FIFO_q_p zombie_queue = fifo_q_new();
+	FIFO_q_p* the_mlfq = priority_queue();
 	
 	// Pointer for currently running process
 	PCB_p current = NULL;
@@ -32,7 +36,7 @@ int main(void) {
 
 	
 	// Main loop in which each cycle/iteration represents a timer quantum
-	while (jobs->length < 30) {
+	for (;;) {
 		
 		// Function to create a random number of processes (0 - 5)
 		// Processes are then put in the ready queue
@@ -51,11 +55,21 @@ int main(void) {
 		}
 		
 		
+		
+		
 		// Pseudo-push of PC to system stack
 		sys_stack = pc;
 		
+		int is_terminate = rand()%100;
+		if(is_terminate <= 15 /* && !privileged_few*/)
+		{
+			enum state_type status = HALTED;
+			pcb_set_state(current, status);
+		}
+		
 		// Simulate timer interrupt / call a pseudo-ISR
-		isr(jobs, current, &pc);
+		/*	Call this when job uses up alloted quantum for its priority level	*/
+		isr(jobs, current, &pc); // TODO: change to accept MLFQ
 		printf("-------------------\n"); //For loop tracking
 	}
 	
@@ -70,17 +84,18 @@ void create_processes(FIFO_q_p jobs) {
 	/* Start creating and adding the jobs to the queue */
 	int i;
 	PCB_p newjob;
-	enum interrupt_type status = READY;
+	enum state_type status = NEW;
 	for(i=0; i<n; i++)
 	{
 		newjob = pcb_new();
 		pcb_set_state(newjob, status);
+		pcb_assign_pid(newjob);
 		fifo_q_enqueue(jobs, newjob);
 	}
 	printf("Jobs created\n");
 }
 
-void isr(FIFO_q_p jobs, PCB_p current, unsigned int * pc) {
+void isr(FIFO_q_p jobs, PCB_p current, unsigned int * pc, FIFO_q_p* mlfq) {
 	// ISR will change the state of the running process to interrupted,
 	// save the CPU state (PC) to the PCB for that process, and do an up-call (scheduler)
 	if(current != NULL)
@@ -88,15 +103,22 @@ void isr(FIFO_q_p jobs, PCB_p current, unsigned int * pc) {
 		current->state = INTERRUPTED;
 		current->context->pc = *pc;
 	}
-	scheduler(TIMER, jobs, current);
+	scheduler(TIMER, jobs, current, mlfq);
 	// Performs pseudo-IRET/returns to main loop
 }
 
-/*  TODO: Decide what parameter should be passed to scheduler to denote
-	a tiimer interrupt. */
-void scheduler(enum interrupt_type interrupt, FIFO_q_p jobs, PCB_p current) {
+/*  TODO: Alter scheduler to accept a priority queue object and implement rules
+	for checking priority acording to MLFQ rules */
+void scheduler(enum interrupt_type interrupt, FIFO_q_p special_q, PCB_p current, FIFO_q_p* mlfq) {
 	// Determines that the scheduler only needs to put the process back
 	// in the ready queue and change its state from interrupted to ready.
+	 
+	if (current->state == HALTED)
+	{
+		fifo_q_enqueue(special_q, current); 
+		current = NULL;
+	}
+	
 	if (interrupt == TIMER)
 	{
 	//	dispatcher(jobs, current);
@@ -104,7 +126,7 @@ void scheduler(enum interrupt_type interrupt, FIFO_q_p jobs, PCB_p current) {
 	// Each time a process is scheduled, print a message with its contents
 		if(current == NULL)
 		{
-			printf("Assigning first job!\n");
+			printf("Assigning new job!\n");
 		}
 		else
 		{
@@ -116,18 +138,18 @@ void scheduler(enum interrupt_type interrupt, FIFO_q_p jobs, PCB_p current) {
 	// Calls dispatcher
 	// At every fourth context switch/call to dispatcher, print contents of running PCB
 	// and "Switching to: " contents of ready queue head
-		if(output_tick==4)
+		/*if(output_tick==4)
 		{
 			char sys_message[256] = "";
 		
 			printf("===============\n");
 			printf("Currently running: %s\n", pcb_to_string(current, sys_message));
 			printf("Switching to: %s\n", pcb_to_string(jobs->front->process, sys_message));
-		}
+		}*/
 		
 		dispatcher(jobs, current);
 		
-		if(output_tick==4)
+		/*if(output_tick==4)
 		{
 			char sys_message[256] = "";
 		
@@ -136,7 +158,7 @@ void scheduler(enum interrupt_type interrupt, FIFO_q_p jobs, PCB_p current) {
 			printf("Contents of queue: %s\n", fifo_q_to_string(jobs, sys_message));
 			printf("===============\n");
 			output_tick = 0;
-		}
+		}*/
 		output_tick++;
 	// After context switch, print the same two PCBs to show different contents
 	// Also print ready queue
@@ -145,7 +167,7 @@ void scheduler(enum interrupt_type interrupt, FIFO_q_p jobs, PCB_p current) {
 	}
 }
 
-void dispatcher(FIFO_q_p jobs, PCB_p current) {
+void dispatcher(FIFO_q_p* mlfq, PCB_p current) {
 	// Will dequeue next waiting process, change to RUNNING
 	// Will copy new pcb's pc value to system stack
 	// Returns to scheduler
