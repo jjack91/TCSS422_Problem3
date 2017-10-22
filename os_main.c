4cef0d67dc54dbcda8df767db08df6fbcd44ebf0
joshua lau <jtml@uw.edu>
2017-10-22 00:39:38 -0700

    worked on fixing main and pqueue

--- os_main.c
+++ os_main.c
@@ -21,7 +21,7 @@ int curr_quantum = 1;
 
 enum interrupt_type {TIMER, IO, EXCEPTION}; // IO and Exception states won't be used (yet),
 											// but includded for completeness
-void create_processes(FIFO_q_p, FIFO_q_p);
+void create_processes(FIFO_q_p, FIFO_q_p, int[]);
 PCB_p nextjob(FIFO_q_p*);
 void choose_special_ids(int[]);
 void isr(FIFO_q_p, PCB_p, unsigned int * pc, FIFO_q_p*, FIFO_q_p);
@@ -53,7 +53,7 @@ int main(void) {
 		
 		// Function to create a random number of processes (0 - 5)
 		// Processes are then put in the ready queue
-		create_processes(new_jobs, special_pcbs);
+		create_processes(new_jobs, special_pcbs, special);
 		
 		// Loop will simulate running of current process
 		// Add a PC value (unsigned int) to current PCB (random between 3000 and 4000)
@@ -61,10 +61,13 @@ int main(void) {
 		
 		if(current == NULL)
 		{
+			char *tostring;
 			printf("Assigning first job\n");
 			current = fifo_q_dequeue(new_jobs);
 			current->context->pc = pc;
-			printf("Job assigned\ncurrent = %d\n", current);
+			printf("Job assigned\ncurrent = ");
+
+			//printf("%s", pcb_to_string(current, tostring));
 		}
 		
 		int i;
@@ -73,7 +76,7 @@ int main(void) {
 			pc += rand()%(1000) + 500;
 			// Pseudo-push of PC to system stack
 			sys_stack = pc;
-		
+			//printf("Job assigned\ncurrent = ");
 			int is_terminate = rand()%100;
 			if(is_terminate <= 15 && (current->pid != special[0]
 				|| current->pid != special[1]
@@ -83,11 +86,13 @@ int main(void) {
 				enum state_type status = HALTED;
 				pcb_set_state(current, status);
 			}
+			//printf("Job assigned\ncurrent = ");
 		
 		}
 		
 		// Simulate timer interrupt / call a pseudo-ISR
 		/*	Call this when job uses up alloted quantum for its priority level	*/
+		//printf("Job assigned\ncurrent = ");
 		isr(zombie_queue, current, &pc, the_mlfq, new_jobs); // TODO: change to accept MLFQ
 		printf("-------------------\n"); //For loop tracking
 		if (output_tick >= MAX_LOOPS)
@@ -107,7 +112,7 @@ void choose_special_ids(int arr[]) {
 	}
 }
 
-void create_processes(FIFO_q_p new_jobs_q, FIFO_q_p privileged) {
+void create_processes(FIFO_q_p new_jobs_q, FIFO_q_p privileged, int special[]) {
 	srand(time(NULL));
 	int n = rand()%6; // get random number of jobs, 0-5
 	printf("Creating jobs...\n");
@@ -120,15 +125,17 @@ void create_processes(FIFO_q_p new_jobs_q, FIFO_q_p privileged) {
 		newjob = pcb_new();
 		pcb_set_state(newjob, status);
 		pcb_assign_pid(newjob);
-		int* val = std::find(std::begin(special), std::end(special), newjob->pid);
-		if (val != std::end(special)) {
-			fifo_q_enqueue(privileged, newjob);
+
+		for(j = 0; j<3; j++) {
+			int val = special[j];
+			if (val == newjob->pid) {
+				fifo_q_enqueue(privileged, newjob);
+				break;
+
+			}
 
 		}
-		else
-		{
-			fifo_q_enqueue(new_jobs_q, newjob);
-		}
+		fifo_q_enqueue(new_jobs_q, newjob);
 	}
 	printf("Jobs created\n");
 }
@@ -136,11 +143,13 @@ void create_processes(FIFO_q_p new_jobs_q, FIFO_q_p privileged) {
 void isr(FIFO_q_p terminate_q, PCB_p current, unsigned int * pc, FIFO_q_p* mlfq, FIFO_q_p new_jobs_q) {
 	// ISR will change the state of the running process to interrupted,
 	// save the CPU state (PC) to the PCB for that process, and do an up-call (scheduler)
+
 	if(current != NULL && current->state != HALTED)
 	{
 		current->state = INTERRUPTED;
 		current->context->pc = *pc;
 	}
+
 	scheduler(TIMER, terminate_q, current, mlfq, new_jobs_q);
 	// Performs pseudo-IRET/returns to main loop
 }
@@ -150,7 +159,19 @@ void isr(FIFO_q_p terminate_q, PCB_p current, unsigned int * pc, FIFO_q_p* mlfq,
 void scheduler(enum interrupt_type interrupt, FIFO_q_p special_q, PCB_p current, FIFO_q_p* mlfq, FIFO_q_p new_jobs_q) {
 	// Determines that the scheduler only needs to put the process back
 	// in the ready queue and change its state from interrupted to ready.
-	 
+	/*	Check for and schedule new jobs	*/
+	
+	if (new_jobs_q->length > 0)
+	{
+		PCB_p tmp = NULL;
+		while (new_jobs_q->length > 0)
+		{
+			tmp = fifo_q_dequeue(new_jobs_q);
+			enqueue_ready(mlfq, 0, tmp);
+		}
+	}
+	
+
 	if (current->state == HALTED)
 	{
 		fifo_q_enqueue(special_q, current); 
@@ -176,22 +197,13 @@ void scheduler(enum interrupt_type interrupt, FIFO_q_p special_q, PCB_p current,
 	}
 	
 	}
-	/*	Check for and schedule new jobs	*/
-	if (new_jobs_q->length > 0)
-	{
-		PCB_p tmp = NULL;
-		while (new_jobs_q->length > 0)
-		{
-			tmp = fifo_q_dequeue(new_jobs_q);
-			enqueue_ready(mlfq, tmp->priority, tmp);
-		}
-	}
 	
 	/*	Kill off zombie jobs if more than 10 exist	*/
 	if (special_q->length >= 10)
 	{
 		while (special_q->length > 0)
 		{
+
 			fifo_q_dequeue(special_q);
 		}
 	}
@@ -203,6 +215,7 @@ void dispatcher(FIFO_q_p* mlfq, PCB_p current) {
 	// Will dequeue next waiting process, change to RUNNING
 	// Will copy new pcb's pc value to system stack
 	// Returns to scheduler
+
 	if(current != NULL)
 	{
 		//printf("current is non-null\n");
@@ -211,14 +224,22 @@ void dispatcher(FIFO_q_p* mlfq, PCB_p current) {
 		if(current->priority < 15) {
 			current->priority += 1;
 		}
+		//printf("Job assigned\nairplane = ");
 		enqueue_ready(mlfq, current->priority, current);
+		//printf("Job assigned\nfairplane = ");
 	}
+	//intf("Job assigned\nairplane = ");
 		//current = fifo_q_dequeue(jobs);
+		//printf("Job assigned\nairplane = ");
 		current = nextjob(mlfq);
-		current->state = RUNNING;
-		//printf("Switched state to running\n");
-		/* Pull data from PCB and hand off to system */
-		sys_stack = current->context->pc;
+		if(current != NULL) {
+
+			current->state = RUNNING;
+			//printf("Switched state to running\n");
+			/* Pull data from PCB and hand off to system */
+			sys_stack = current->context->pc;
+			printf("Job assigned\nairplane = ");
+		}
 		/* return to scheduler */ 
 }
 
@@ -227,7 +248,8 @@ PCB_p nextjob(FIFO_q_p* mlfq) {
 	int i;
 	for(i = 0; i < 15; i++) {
 		nj = dequeue_ready(mlfq, i);
-		if(nj!= NULL)
-			break;
+		if(nj!= NULL) break;
 	}
+		return nj;
+	
 }
